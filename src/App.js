1de96ed8cb8f409d055927570560f41bb0658c47
DinoSMZ <dinoaldair@hotmail.com>
2022-09-03 10:09:49 -0500

    prert

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import React from 'react';
-import AppRouter from './routers/AppRouter';
-import "./Style.css"
+import React from "react";
+import AppRouter from "./routers/AppRouter";
+import "./Style.css";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-
-
 const App = () => {
-    return ( <
-        >
-
-        <
-        AppRouter / >
-
-        <
-        />
-
-    );
+  return (
+    <>
+      <AppRouter />
+    </>
+  );
 };
 
 export default App;
